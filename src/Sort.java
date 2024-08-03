35bf8a3f4ed756aa3155d57ed5cd6dfabeaad0d1
Mounika Gaddamuri <Mounika.Gaddamuri@godigit.com>
2024-08-03 15:47:40 +0530

    sorting based on age

--- src/Sort.java
+++ src/Sort.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 class Person{
     String name;
@@ -15,10 +16,13 @@ public class Sort {
     public static void main(String[] args) {
 
         List<Person> list = new ArrayList<>();
-        list.add(new Person("def",21,123.4f));
+        list.add(new Person("def",30,123.4f));
         list.add(new Person("abc",22,125.4f));
         list.add(new Person("xyz",25,126.4f));
 
+        list.stream().sorted(Comparator.comparingInt(p -> p.age)).forEach(s-> System.out.println(s.name+" "+s.age+" "+s.salary));
+
+        //sortedByAge.forEach(System.out::println);
 
     }
 }
